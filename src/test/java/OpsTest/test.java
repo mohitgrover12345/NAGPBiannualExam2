f3493f90ddb5587bed0ee32b2420a089b49cc282
Mohit Grover <mohit.grover@mycom-osi.com>
2022-02-23 12:50:46 +0530

    Adding Files

---
+++ src/test/java/OpsTest/test.java
@@ -0,0 +1,18 @@
+package OpsTest;
+
+import org.junit.Test;
+
+public class test {
+
+	@Test
+	public void tc001()
+	{
+		System.out.println("First test case for building jenkins pipeline");
+	}
+	
+	@Test
+	public void tc002()
+	{
+		System.out.println("second test case for building jenkins pipeline");
+	}
+}
